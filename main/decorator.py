7744772274be1b89be5851311009172995d12cd0
Arcanjo Marcelino,Cynthia Kenia <cynthiakenia.arcanjomarcelino@drei.com>
2020-06-23 15:20:39 +0200

    #2 data api

--- main/decorator.py
+++ main/decorator.py
@@ -5,22 +5,38 @@ from kubernetes.client import V1Pod
 from minio import Minio
 from minio.error import ResponseError
 
+from data.minio import download_file, upload_file
 
 
 def consume(urn):
     def wrapper(func):
-        logging.info('Consume.wrapper(%s)'% func)
+        logging.info('Consume.wrapper(%s)' % func)
+
         def call(*args, **kwargs):
-            logging.info('Consume.call(%s,%s,%s)'% (func,args,kwargs))
-            post_data = json.dumps(s3_config['consume'])
-            logging.info('POST Data(%s)' % post_data)
-            response = requests.get(url=url,headers={'content-type':'application/json'},data=post_data)
-            logging.info('Response (%s)'% response)
-            if response.status_code!=200:
-                sys.exit("Error calling wrapper, expected: %d, got: %d\n" % (200, response.status_code))
-            logging.info('Response Data(%s)' % response.text)
-            kwargs['data'] = response.text
+            logging.info('Consume.call(%s,%s,%s)' % (func, args, kwargs))
+            content = download_file(urn)
+            logging.info('Content Data(%s)' % content)
+            kwargs['data'] = content
             return func(*args, **kwargs)
+
         logging.debug('Consume.wrapper over')
         return call
+
+    return wrapper
+
+
+def produce(urn):
+    def wrapper(func):
+        logging.info('Produce.wrapper(%s)' % func)
+
+        def call(*args, **kwargs):
+            logging.info('Produce.call(%s,%s,%s)' % (func, args, kwargs))
+            response = func(*args, **kwargs)
+            upload_file(urn,response)
+            logging.info('Produce.store(%s)' % response)
+            return response
+
+        logging.debug('Produce.wrapper over')
+        return call
+
     return wrapper
--- main/minio.py
+++ main/minio.py
@@ -1,72 +1,88 @@
 import os
 import logging
-
+from typing import List
 
 from kubernetes import config, watch, client
 from kubernetes.client import V1Pod
 from minio import Minio
 from minio.error import ResponseError
 
+from data.utils import get_bucket_urn, get_file_name_urn
 
-def find_minio_pods(bucket: str, file_name: str) -> None:
-    logging.debug('resolve minio pods...')
+
+def list_minio_pods() -> List[str]:
+    logging.debug('list minio pods...')
     api = client.CoreV1Api()
     # field selectors are a string, you need to parse the fields from the pods here
     app = 'minio'
     ret = api.list_pod_for_all_namespaces(watch=False, label_selector="app=" + app)
+    pods = list()
     for i in ret.items:
         for c in filter(lambda co: co.name == app, i.spec.containers):
             for p in c.ports:
-                logging.info('ip: %s, port: %s' % (i.status.pod_ip, p.container_port))
-                logging.info(
-                    'pod_has_file: %s' % has_pod_file(bucket, file_name, str(i.status.pod_ip), str(p.container_port)))
-                list_objects_minio(i.status.pod_ip, p.container_port,bucket)
-                list_objects_minio(i.status.pod_ip, p.container_port)
+                pods.append("%s:%d" % (i.status.pod_ip, p.container_port))
+    logging.debug(pods)
+    return pods
 
 
-def is_storage_pod(pod: V1Pod) -> bool:
-    logging.debug('resolve minio pods...')
-    # field selectors are a string, you need to parse the fields from the pods here
-    app = 'minio'
-    return any(filter(lambda co: co.name == app, pod.spec.containers))
-
-
-def list_objects_minio(ip: str, port: str, bucket=None) -> None:
+def has_pod_file(urn: str, minio_addr: str) -> bool:
     try:
-        client = minio_client(ip, port)
-        if not bucket:
-            buckets = minio_client(ip, port).list_buckets()
-            for b in buckets:
-                logging.info('List files in bucket  : %s' % b.name)
-                objects = client.list_objects(b.name, recursive=True)
-                for obj in objects:
-                    logging.info('File Name: %s, Size: %s'%(obj.object_name.encode('utf-8'), obj.size))
+        if has_pod_bucket(get_bucket_urn(urn),minio_addr):
+            stat = minio_client(minio_addr).stat_object(get_bucket_urn(urn), get_file_name_urn(urn))
+            return stat.size > 0
         else:
-            logging.info('List files in bucket  : %s'% bucket)
-            objects = client.list_objects(str(bucket), recursive=True)
-            for obj in objects:
-                logging.info('File Name: %s, Size: %s'%(obj.object_name.encode('utf-8'), obj.size))
+            return False
     except ResponseError as err:
-        logging.exception('MinioClientException: %s', err)
+        logging.warning('MinioClientException: %s', err.message)
+        return False
 
 
-def has_pod_file(bucket: str, file_name: str, ip: str, port: str) -> bool:
+def has_pod_bucket(bucket: str, minio_addr: str) -> bool:
     try:
-        stat = minio_client(ip, port).stat_object(bucket, file_name)
-        return stat.size > 0
+        return minio_client(minio_addr).bucket_exists(bucket)
     except ResponseError as err:
-        logging.exception('MinioClientException: %s', err.message)
+        logging.error('MinioClientException: %s', err.message)
+        return False
 
 
-def minio_client(ip: str, port: str) -> Minio:
+def minio_client(minio_addr: str) -> Minio:
     minio_ac = os.environ.get('MINIO_AC', None)
     minio_sc = os.environ.get('MINIO_SC', None)
-    logging.debug('Minio ACCESS_KEY: %s'% minio_ac)
-    logging.debug('Minio SECRET_KEY: %s'% minio_sc)
-    minio_addr = ('%s:%s'%(ip, port))
-    logging.debug('Connecting to pod : %s'% minio_addr)
+    logging.debug('Minio ACCESS_KEY: %s' % minio_ac)
+    logging.debug('Minio SECRET_KEY: %s' % minio_sc)
+    logging.debug('Connecting to pod : %s' % minio_addr)
     client = Minio(minio_addr,
                    access_key=minio_ac,
                    secret_key=minio_sc,
                    secure=False)
     return client
+
+
+def download_file(urn: str) -> str:
+    logging.info('download file from urn  %s' % urn)
+    for minio_addr in list_minio_pods():
+        if has_pod_file(urn, minio_addr):
+            client = minio_client(minio_addr)
+            response = client.get_object(get_bucket_urn(urn), get_file_name_urn(urn))
+            content = str(response.read().decode('utf-8'))
+            logging.debug('file content: %s' % content)
+            return content
+
+
+def upload_file(urn: str, content: str) -> None:
+    logging.info('upload urn  %s' % urn)
+    _wfile_name = get_file_name_urn(urn)
+    text_file = open(_wfile_name, "wt")
+    n = text_file.write(content)
+    text_file.close()
+    try:
+        with open(_wfile_name, 'rb') as file_data:
+            for minio_addr in list_minio_pods():
+                if has_pod_bucket(get_bucket_urn(urn), minio_addr):
+                    client = minio_client(minio_addr)
+                    file_stat = os.stat(_wfile_name)
+                    client.put_object(get_bucket_urn(urn), _wfile_name, file_data, file_stat.st_size,
+                                      content_type='application/json')
+                    break
+    except ResponseError as e:
+        logging.error('MinioClientException: %s', e.message)
