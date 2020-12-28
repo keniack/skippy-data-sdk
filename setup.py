e2296f5fee19aae7f66a8f7b048e8b9455fb1053
Cynthia Marcelino <keniack@gmail.com>
2020-12-28 21:16:18 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open("requirements.txt", "r") as fh:
-    install_requires = [line for line in fh.read().split(os.linesep) if line]
-
 setuptools.setup(
     name="skippy-data",
-    version="0.1",
+    version="0.3",
     author="Cynthia Marcelino",
     author_email="keniack@gmail.com",
     description="look up data",
@@ -19,7 +16,7 @@ setuptools.setup(
     download_url="https://git.dsg.tuwien.ac.at/c.marcelino/skippy-data",
     packages=setuptools.find_packages(),
     setup_requires=['wheel'],
-    install_requires=install_requires,
+    install_requires=['kubernetes~=12.0.1','minio~=7.0.0','redis~=3.5.3','PyYAML~=5.3.1'],
     pyton_requires='>=3.7',
     classifiers=[
         "Programming Language :: Python :: 3",
